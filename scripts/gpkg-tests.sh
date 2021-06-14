26427ec956ad3a8a2f72095f11e5cbb236662b89
benatagirrea <benatagirrea@gmail.com>
2021-06-14 22:35:57 +0200

    Actualizar memoria y scripts

---
+++ scripts/gpkg-tests.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+shopt -s globstar
+
+if [ $# -ne 2 ]; then
+    echo "Incorrect arguments"
+    echo "  gpkg-tests.sh - run ktr transformations and/or check ttl results"
+    echo "  cat [run/check] [DIRECTORY]"
+    exit
+fi
+
+#start="../btn100-master-geopackage/transformaciones-shape/1-UnidadesAdministrativas/2-ZonaProtegida"
+start="$2"
+
+if [ $1 == "run" ]; then
+    rm gpkg-tests-pan.log
+    echo "counting transformations..."
+    i=0
+    for f in $start/**/*.ktr ; do
+        i=$((i+1))
+    done
+    echo "$i ktr files to process"
+
+    j=0
+    echo "running gpkg transformations..."
+    for f in $start/**/*.ktr ; do
+        pan -file=$f >> gpkg-tests-pan.log 2>&1 
+        j=$((j+1))
+        echo "$j/$i $f"
+    done
+    echo "done"
+fi
+
+echo "finding differences in gpkg transformation results..."
+for f in $start/**/*.ttl ; do
+    cmp $f $(sed 's/btn100-master-geopackage/btn100-master/g' <<< $f)
+done
+echo "done"
--- scripts/shpTogpkg.sh
+++ scripts/shpTogpkg.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 shopt -s globstar
-start="../btn100-master/transformaciones-shape/1-UnidadesAdministrativas"
+start="../btn100-master-geopackage/transformaciones-shape"
+echo "converting shp files to gpkg..."
 for f in $start/**/*.shp ; do
-    echo "f"
-    ogr2ogr -f "GPKG" "${f%.*}.gpkg" "$f" -nln Table
+    echo "$f"
+    ogr2ogr -f "GPKG" "${f%.*}.gpkg" "$f" -nln Table -nlt MULTIPOLYGON
 done
+echo "done"
+
+echo "converting ktr to gpkg..."
+for f in $start/**/*.ktr ; do
+    echo "$f"
+    sed -e 's/<from>shp/<from>gpkg/g' \
+        -e 's/<name>shp/<name>gpkg/g' \
+        -e 's/<inputFormat>ESRI_SHP/<inputFormat>GEOPACKAGE/g' \
+        -e 's:<key>FORCE_TO_2D:<key>DB_TABLE_NAME</key> <value>Table</value> </param> <param> <key>FORCE_TO_2D:g' \
+        -e 's:shp</inputFileName>:gpkg</inputFileName>:g' \
+        -e 's/<encoding>ISO-8859-1/<encoding>UTF-8/g' \
+        -i $f
+done
+echo "done"
